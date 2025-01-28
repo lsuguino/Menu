98ca6c639140f08379306d12f1b43f9a1ad1594c
Leonardo Suguino <leonardo_suguino@hotmail.com>
2025-01-28 14:56:03 -0300

    Update on main

--- database.py
+++ database.py
@@ -1,6 +1,10 @@
 buttons = {
     'Starters': {
-        'dish_name':['Chicken Salad'],
-        'dish_description': ['Mix of leafs with onions, tomatoes, cheese and chicken'],
+        'dish_name2':['Chicken Salad', 'Fried Shrimps'],
+        'dish_description2': ['Mix of leafs with onions, tomatoes, cheese and chicken', 'Big shrimps fried'],
+    },
+      'Dishes': {
+        'dish_name2':['Chicken Salad', 'Fried Shrimps'],
+        'dish_description2': ['Mix of leafs with onions, tomatoes, cheese and chicken', 'Big shrimps fried'],
     }
 }
